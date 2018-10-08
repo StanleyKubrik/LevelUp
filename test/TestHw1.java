9f1ad1bfd82337a0fa66035caf166d85bdcef04a
Roman <kvlad.mail@gmail.com>
2018-10-08 20:54:53 +0300

    Courses0810

--- test/TestHw1.java
+++ test/TestHw1.java
@@ -1,2 +1,18 @@
+import org.junit.After;
+import org.junit.Before;
+
 public class TestHw1 {
+    private CondStatements cs;
+
+    @Before
+    public void setupUp(){
+        cs = new CondStatements();
+    }
+
+    @After
+    public void setupDown(){
+        cs = null;
+    }
+
+
 }
