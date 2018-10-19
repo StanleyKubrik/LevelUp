3441d4a1ab0e91675d6ee833156feae9d2932cab
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-10-19 17:31:36 +0300

    Work191018

--- src/ConsoleCalc.java
+++ src/ConsoleCalc.java
@@ -9,26 +9,35 @@ public class ConsoleCalc {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         do {
-            System.out.println("Введите код операции: ");
+            System.out.println("Введите код операции:");
             code = bufferedReader.readLine();
-            System.out.println("Введите 1-е число: ");
-            number1 = Integer.parseInt(bufferedReader.readLine());
-            System.out.println("Введите 2-е число: ");
-            number2 = Integer.parseInt(bufferedReader.readLine());
 
-            switch (code){
-                case "+":
-                    System.out.println("Ответ: " + (number1 + number2));
-                    break;
-                case "-":
-                    System.out.println("Ответ: " + (number1 - number2));
-                    break;
-                case "*":
-                    System.out.println("Ответ: " + (number1 * number2));
-                    break;
-                case "/":
-                    System.out.println("Ответ: " + (number1 / number2));
-                    break;
+            if (code.equals("+") || code.equals("-") || code.equals("*") || code.equals("/") || code.equals("%")) {
+                System.out.println("Введите 1-е число:");
+                number1 = Integer.parseInt(bufferedReader.readLine());
+                System.out.println("Введите 2-е число:");
+                number2 = Integer.parseInt(bufferedReader.readLine());
+
+                switch (code){
+                    case "+":
+                        System.out.println("Ответ: " + (number1 + number2));
+                        break;
+                    case "-":
+                        System.out.println("Ответ: " + (number1 - number2));
+                        break;
+                    case "*":
+                        System.out.println("Ответ: " + (number1 * number2));
+                        break;
+                    case "/":
+                    case "%":
+                        if (number2 == 0){
+                            System.out.println("ОШИБКА: На ноль делить нельзя!");
+                        } else {
+                            System.out.println("Ответ: " + (number1 / number2));
+                            break;
+                        }
+                }
+            } else {
             }
         } while (!code.equalsIgnoreCase("exit"));
     }
