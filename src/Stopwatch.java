a2aa58888a22f86d643a645eb71309bf22a4911e
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-11-03 13:59:47 +0200

    Home031118

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,27 +1,43 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Stopwatch {
-//    private int hours = 0;
-//    private int minutes = 0;
-//    private int seconds = 1;
-//
-//    public void fiveMin() throws InterruptedException {
-//        for (; seconds <= 60; seconds++) {
-//            if (seconds == 60) {
-//                seconds = 0;
-//                minutes++;
-//            }
-//            if (minutes == 5 && seconds == 1){
-//                return;
-//            }
-//            System.out.println(hours + ":" + minutes + ":" + seconds);
-//            Thread.sleep(1000);
-//        }
-//    }
+    private int hours = 0;
+    private int minutes = 0;
+    private int seconds = 1;
 
-    private Date date;
+    public void fiveMin() throws InterruptedException {
+        Date date = new Date();
+        date.setHours(0);
+        date.setMinutes(0);
+        date.setSeconds(0);
 
-    public void fiveMin(){
+        for (; seconds <= 60; seconds++) {
+            if (seconds == 60) {
+                date.setSeconds(0);
+                seconds = 0;
+                minutes++;
+            }
+            if (minutes == 60) {
+                date.setMinutes(0);
+                minutes = 0;
+                hours++;
+            }
+            if (minutes == 5 && seconds == 1){
+                return;
+            }
+            date.setSeconds(seconds);
+            date.setMinutes(minutes);
 
+            System.out.println(formatTime(date.getTime()));
+            Thread.sleep(10);
+        }
+    }
+
+    static String formatTime(long ms) {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss") ;
+        format.setTimeZone(TimeZone.getTimeZone("EET"));
+        return format.format(new Date(ms));
     }
 }
