a612fa66ef86e1c1e8b5028497363f42c103be3b
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-11-03 13:04:06 +0200

    Home031118

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,19 +1,27 @@
-public class Stopwatch {
-    private int hours = 0;
-    private int minutes = 0;
-    private int seconds = 1;
+import java.util.Date;
 
-    public void fiveMin() throws InterruptedException {
-        for (; seconds <= 60; seconds++) {
-            if (seconds == 60) {
-                seconds = 0;
-                minutes++;
-            }
-            if (minutes == 5 && seconds == 1){
-                return;
-            }
-            System.out.println(hours + ":" + minutes + ":" + seconds);
-            Thread.sleep(1);
-        }
+public class Stopwatch {
+//    private int hours = 0;
+//    private int minutes = 0;
+//    private int seconds = 1;
+//
+//    public void fiveMin() throws InterruptedException {
+//        for (; seconds <= 60; seconds++) {
+//            if (seconds == 60) {
+//                seconds = 0;
+//                minutes++;
+//            }
+//            if (minutes == 5 && seconds == 1){
+//                return;
+//            }
+//            System.out.println(hours + ":" + minutes + ":" + seconds);
+//            Thread.sleep(1000);
+//        }
+//    }
+
+    private Date date;
+
+    public void fiveMin(){
+
     }
 }
